b6162ef46e5571ef43d8f48af3a211b2b5f93552
kathling <talk2keling@gmail.com>
2021-05-22 19:21:24 -0400

    getters and setters

--- com/test/Employee.java
+++ com/test/Employee.java
@@ -1,10 +1,32 @@
 package com.test;
 
 public class Employee {
-    public int baseSalary;
-    public int hourlyRate;
+    private int baseSalary;
+    private int hourlyRate;
 
     public int calculateWage(int extraHours) {
         return baseSalary + (hourlyRate * extraHours);
+    }
+
+    // Setters
+    public void setBaseSalary(int baseSalary) {
+        if (baseSalary <= 0)
+            throw new IllegalArgumentException("Salary cannot be 0 or less");
+        this.baseSalary = baseSalary;
+    }
+
+    public void setHourlyRate(int hourlyRate) {
+        if (hourlyRate <= 0)
+            throw new IllegalArgumentException("Hourly rate cannot be 0 or negative");
+        this.hourlyRate = hourlyRate;
+    }
+
+    // Getters
+    public int getBaseSalary() {
+        return baseSalary;
+    }
+
+    public int getHourlyRate() {
+        return hourlyRate;
     }
 }
--- com/test/t.java
+++ com/test/t.java
@@ -6,8 +6,8 @@ public class t {
 
     public static void main(String[] args) {
         var employee = new Employee();
-        employee.baseSalary = 50_000;
-        employee.hourlyRate = 20;
+        employee.setBaseSalary(50_000);
+        employee.setHourlyRate(20);
         int wage = employee.calculateWage(10);
         System.out.println(wage);
     }
