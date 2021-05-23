714e90819d09a6b1832ed97618363af76c094dc5
kathling <talk2keling@gmail.com>
2021-05-22 22:09:50 -0400

    add constructor

--- com/test/Employee.java
+++ com/test/Employee.java
@@ -4,29 +4,34 @@ public class Employee {
     private int baseSalary;
     private int hourlyRate;
 
+    public Employee(int baseSalary, int hourlyRate) {
+        setBaseSalary(baseSalary);
+        setHourlyRate(hourlyRate);
+    }
+
     public int calculateWage(int extraHours) {
         return baseSalary + (hourlyRate * extraHours);
     }
 
     // Setters
-    public void setBaseSalary(int baseSalary) {
+    private void setBaseSalary(int baseSalary) {
         if (baseSalary <= 0)
             throw new IllegalArgumentException("Salary cannot be 0 or less");
         this.baseSalary = baseSalary;
     }
 
-    public void setHourlyRate(int hourlyRate) {
+    private void setHourlyRate(int hourlyRate) {
         if (hourlyRate <= 0)
             throw new IllegalArgumentException("Hourly rate cannot be 0 or negative");
         this.hourlyRate = hourlyRate;
     }
 
     // Getters
-    public int getBaseSalary() {
+    private int getBaseSalary() {
         return baseSalary;
     }
 
-    public int getHourlyRate() {
+    private int getHourlyRate() {
         return hourlyRate;
     }
 }
--- com/test/t.java
+++ com/test/t.java
@@ -5,12 +5,8 @@ import java.util.Scanner;
 public class t {
 
     public static void main(String[] args) {
-        var employee = new Employee();
-        employee.setBaseSalary(50_000);
-        employee.setHourlyRate(20);
+        var employee = new Employee(50_000, 20);
         int wage = employee.calculateWage(10);
         System.out.println(wage);
-
-        System.out.println("hello");
     }
 }
