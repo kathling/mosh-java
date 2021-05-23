39b05b0f30c0b389bf3210d8b6d411961e6a3f94
kathling <talk2keling@gmail.com>
2021-05-23 04:28:40 -0400

    upcasting and downcasting

--- com/test/Main1.java
+++ com/test/Main1.java
@@ -2,10 +2,16 @@ package com.test;
 
 public class Main1 {
     public static void main(String[] args) {
+        var control = new UIControl(true);
         var textBox = new TextBox();
-        textBox.setText("Hello World");
-        System.out.println(textBox.toString());
-        System.out.println(textBox);
+        show(textBox);
     }
 
+    public static void show(UIControl control) {
+        if (control instanceof TextBox) {
+            var textBox = (TextBox) control;
+            textBox.setText("Hello World");
+        }
+        System.out.println(control);
+    }
 }
