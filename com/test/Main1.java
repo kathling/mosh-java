dd487983459c7034cea92769f68f616292b9b91f
kathling <talk2keling@gmail.com>
2021-05-23 04:45:46 -0400

    comparing objects

--- com/test/Main1.java
+++ com/test/Main1.java
@@ -2,16 +2,9 @@ package com.test;
 
 public class Main1 {
     public static void main(String[] args) {
-        var control = new UIControl(true);
-        var textBox = new TextBox();
-        show(textBox);
-    }
-
-    public static void show(UIControl control) {
-        if (control instanceof TextBox) {
-            var textBox = (TextBox) control;
-            textBox.setText("Hello World");
-        }
-        System.out.println(control);
+        var point1 = new Point(1, 2);
+        var point2 = new Point(1, 2);
+        System.out.println(point1.hashCode());
+        System.out.println(point2.hashCode());
     }
 }
