a000b89088be2da2ddbcadff2b9246dda39b547e
kathling <talk2keling@gmail.com>
2021-05-23 04:01:05 -0400

    overriding methods

---
+++ com/test/Main1.java
@@ -0,0 +1,11 @@
+package com.test;
+
+public class Main1 {
+    public static void main(String[] args) {
+        var textBox = new TextBox();
+        textBox.setText("Hello World");
+        System.out.println(textBox.toString());
+        System.out.println(textBox);
+    }
+
+}
